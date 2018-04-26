f6d759d92c1eab5d7fde12e06f5b0793e6720078
suhininalex@gmail.com <l112358132134>
2018-04-26 14:05:10 +0300

    test: add test test for libgdx method index

---
+++ src/test/kotlin/com/github/suhininalex/codefinder/index/PersistedMethodIndexTest.kt
@@ -0,0 +1,32 @@
+package com.github.suhininalex.codefinder.index
+
+import com.github.suhininalex.codefinder.preprocessing.MethodDescription
+import com.github.suhininalex.codefinder.preprocessing.tokens.CallToken
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class PersistedMethodIndexTest {
+
+    val methodIndex = PersistedMethodIndex("src/test/data/libgdx/index/method")
+
+    @Test
+    fun `test libgdx index`() {
+        val methodGetX = methodIndex.getMethodById("com.badlogic.gdx.Input.getX()")
+        assertThat(methodGetX).isNotNull()
+        assertThat(methodGetX?.content).isEmpty()
+        assertThat(methodGetX?.className).isEqualTo("Input")
+    }
+
+    @Test
+    fun `test libgdx usages`() {
+        val methodName = "com.badlogic.gdx.Input.getX()"
+        val usages = methodIndex.getUsagesOf(methodName).mapNotNull { methodIndex.getMethodById(it) }
+        fun MethodDescription.containsCall(qualifiedName: String): Boolean {
+            return content.filterIsInstance<CallToken>().any { it.reference == qualifiedName }
+        }
+        assertThat(usages).isNotEmpty
+        usages.forEach {
+            assertThat(it.containsCall(methodName)).isTrue()
+        }
+    }
+}
