3fa22f8908771e6c8bd5f5a09ce786fd0426706f
suhininalex@gmail.com <l112358132134>
2018-04-26 12:52:22 +0300

    fix: read descriptions from json files

--- src/main/kotlin/com/github/suhininalex/codefinder/preprocessing/JsonReader.kt
+++ src/main/kotlin/com/github/suhininalex/codefinder/preprocessing/JsonReader.kt
@@ -1,20 +1,17 @@
 package com.github.suhininalex.codefinder.preprocessing
 
+import com.github.suhininalex.codefinder.preprocessing.tokens.TokenJsonSerializer
+import com.github.suhininalex.codefinder.utils.findFilesByType
 import java.io.File
 
 class TokenJsonReader(private val file: String){
 
     val files: Sequence<FileDescription>
         get() {
-            File(file).reader().use { input ->
-                val result = TokenJsonSerializer.fromJson(input, FileDescription::class.java)
-                println(result)
+            return File(file).findFilesByType("json").map { file ->
+                file.reader().use { input ->
+                    TokenJsonSerializer.fromJson(input, FileDescription::class.java)
+                }
             }
-            return emptySequence()
         }
-}
-
-fun main(args: Array<String>) {
-    val reader = TokenJsonReader("src/test/data/libgdx/description.json")
-    reader.files
 }
