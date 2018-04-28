5aaf7551776dd3707e63aa3c5a15c57e1bb1643a
suhininalex@gmail.com <l112358132134>
2018-04-28 16:08:44 +0300

    feature: add nice progressbar while indexing methods

--- src/main/kotlin/com/github/suhininalex/codefinder/index/CreateMethodIndex.kt
+++ src/main/kotlin/com/github/suhininalex/codefinder/index/CreateMethodIndex.kt
@@ -1,12 +1,14 @@
 package com.github.suhininalex.codefinder.index
 
 import com.github.suhininalex.codefinder.preprocessing.TokenJsonReader
+import com.github.suhininalex.codefinder.utils.wrapWithProgressBar
 
 fun main(args: Array<String>) {
     require(args.size == 2) { "Input json directory or output index directory is not defined"}
     val (inputJsonDirectory, indexDirectory) = args
     val methodIndex: MethodIndex = PersistedMethodIndex(indexDirectory)
-    TokenJsonReader(inputJsonDirectory).files
+    TokenJsonReader(inputJsonDirectory).files.toList()
+            .wrapWithProgressBar("Building index from json files")
             .flatMap { it.methods.asSequence() }
             .forEach { methodIndex.indexMethod(it) }
 }
